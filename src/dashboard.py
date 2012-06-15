b10c60283fdc6a0b9a5c15f7a8f317389065f6d5
Felix Kaser <f.kaser@gmx.net>
2012-06-15 12:23:16 +1200

    move widgets into submodule

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('rosdashboard')
 import rospy
-from std_msgs.msg import String
-from turtlesim.msg import Pose
 
 import sys
 from PyQt4 import QtGui, QtCore
 
 from modules.props import DashboardProperties, WidgetProperties
+from modules.widgets import DragButton, DragDial
 
 """
 def callback(data, l):
@@ -15,87 +14,6 @@ def callback(data, l):
     l.resize(l.sizeHint())
     rospy.loginfo(rospy.get_name()+"I heard %s",data.data)
 """
-
-class DashboardWidget(QtGui.QGroupBox):
-    """ base class for draggable widgets """
-    
-    """ signal to be emitted when the widget was selected """
-    selected = QtCore.pyqtSignal()
-    
-    def __init__(self, parent):
-        super(DashboardWidget, self).__init__(parent)
-
-    def mouseMoveEvent(self, e):
-
-        mimeData = QtCore.QMimeData()
-
-        drag = QtGui.QDrag(self)
-        drag.setMimeData(mimeData)
-        drag.setHotSpot(e.pos() - self.rect().topLeft())
-
-        dropAction = drag.start(QtCore.Qt.MoveAction)
-        
-    
-    def mouseReleaseEvent(self, e):
-        self.selected.emit()
-        print('mouseReleaseEvent on DashboardWidget')
-        #QtGui.QPushButton.mousePressEvent(self, e)
-        #if e.button() == QtCore.Qt.LeftButton:
-        #    print 'press'
-       
-   
-class DragDial(DashboardWidget):
-    """ draggable dial and lcd display """
-    def __init__(self, parent):
-        super(DragDial, self).__init__(parent)
-        self.setTitle('DragDial')
-        self.initUI()
-        self.initSubscriptions()
-        
-    def initUI(self):
-        self.layout = QtGui.QVBoxLayout()
-        self.dial = QtGui.QDial(self)
-        self.dial.setMinimum(-2)
-        self.dial.setMaximum(2)
-        self.dial.setSliderPosition(0)
-        self.dial.setDisabled(True)
-        
-        self.lcd = QtGui.QLCDNumber(self)
-        self.lcd.setSegmentStyle(QtGui.QLCDNumber.Flat)
-        self.dial.valueChanged.connect(self.lcd.display)
-        
-        self.layout.addWidget(self.dial)
-        self.layout.addWidget(self.lcd)
-        
-        self.setLayout(self.layout)
-        
-    def initSubscriptions(self):
-        rospy.Subscriber("turtle1/pose", Pose, self.subscriptionCallback)
-        
-    def subscriptionCallback(self, data):
-        self.dial.setValue(int(data.linear_velocity))
-        
-class DragButton(DashboardWidget):
-    """ drag button wrapper """
-    def __init__(self, title, parent):
-        super(DragButton, self).__init__(parent)
-        self.setTitle('DragButton')
-        self.initUI(title)
-        
-    def initUI(self, title):
-        
-        self.button = QtGui.QPushButton(title, self)
-        self.button.setDisabled(True)
-        self.button.clicked.connect(self.buttonClicked)
-        
-        self.layout = QtGui.QVBoxLayout()
-        self.layout.addWidget(self.button)
-        
-        self.setLayout(self.layout)
-        
-    def buttonClicked(self):
-        #if event.button() == QtCore.Qt.LeftButton:
-        print 'button clicked'    
 
 
 class Dashboard(QtGui.QGroupBox):
@@ -117,12 +35,12 @@ class Dashboard(QtGui.QGroupBox):
         self.button = DragButton('Button', self)
         self.button.move(100, 65)
         
-        self.button.selected.connect(self.selectionChanged)
+        self.button.rightClicked.connect(self.selectionChanged)
         
         self.dial = DragDial(self)
         self.dial.move(100, 150)
         
-        self.dial.selected.connect(self.selectionChanged)
+        self.dial.rightClicked.connect(self.selectionChanged)
         
         #self.multiple = Multiple('multi', self)
         #self.multiple.move(100, 100)
@@ -152,7 +70,7 @@ class Dashboard(QtGui.QGroupBox):
         
 def main():
     
-    rospy.init_node('python_test', anonymous=True)
+    rospy.init_node('dashboard', anonymous=True)
     
     app = QtGui.QApplication(sys.argv)
     
