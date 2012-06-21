d1d13599694a2ce4a0bf7faca6c5db25a0aba924
Felix Kaser <f.kaser@gmx.net>
2012-06-21 17:06:06 +1200

    experiment with qlistview

--- src/modules/toolbox.py
+++ src/modules/toolbox.py
@@ -11,6 +11,17 @@ class Toolbox(QtGui.QGroupBox):
     def initUI(self):
 
         self.setTitle('Widgets')
+        
+        self.listView = QtGui.QListView(self)
+        
+        list_data = [1,2,3,4]
+        lm = WidgetListModel(list_data, self)
+        self.listView.setModel(lm)
+        
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(self.listView)
+        self.setLayout(self.layout)
+        
         self.setAcceptDrops(True)
         
     def dragEnterEvent(self, e):
@@ -23,3 +34,18 @@ class Toolbox(QtGui.QGroupBox):
 
         e.setDropAction(QtCore.Qt.MoveAction)
         e.accept()
+        
+class WidgetListModel(QtCore.QAbstractListModel): 
+    def __init__(self, data, parent=None): 
+        """ data should be a list of DashboardWidgets """
+        QtCore.QAbstractListModel.__init__(self, parent) 
+        self.listdata = data
+ 
+    def rowCount(self, parent=QtCore.QModelIndex()): 
+        return len(self.listdata) 
+ 
+    def data(self, index, role): 
+        if index.isValid() and role == QtCore.Qt.DisplayRole:
+            return QtCore.QVariant(self.listdata[index.row()])
+        else: 
+            return QtCore.QVariant()
