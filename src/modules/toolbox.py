efa79c6da82139abb51ea76916f3ff9cbc165be0
Felix Kaser <f.kaser@gmx.net>
2012-06-22 14:41:51 +1200

    use listwidget instead of listview

--- src/modules/toolbox.py
+++ src/modules/toolbox.py
@@ -12,40 +12,18 @@ class Toolbox(QtGui.QGroupBox):
 
         self.setTitle('Widgets')
         
-        self.listView = QtGui.QListView(self)
+        self.listWidget = QtGui.QListWidget(self)
         
-        list_data = [1,2,3,4]
-        lm = WidgetListModel(list_data, self)
-        self.listView.setModel(lm)
+        self.listWidget.setDragEnabled(True)
+        self.listWidget.setDropIndicatorShown(True)
+        
+        self.listWidget.addItem("dragbutton")
+        self.listWidget.addItem("dragdial")
         
         self.layout = QtGui.QVBoxLayout()
-        self.layout.addWidget(self.listView)
+        self.layout.addWidget(self.listWidget)
         self.setLayout(self.layout)
-        
-        self.setAcceptDrops(True)
         
     def dragEnterEvent(self, e):
         
         e.accept()
-
-    def dropEvent(self, e):
-
-        e.source().move(e.pos())
-
-        e.setDropAction(QtCore.Qt.MoveAction)
-        e.accept()
-        
-class WidgetListModel(QtCore.QAbstractListModel): 
-    def __init__(self, data, parent=None): 
-        """ data should be a list of DashboardWidgets """
-        QtCore.QAbstractListModel.__init__(self, parent) 
-        self.listdata = data
- 
-    def rowCount(self, parent=QtCore.QModelIndex()): 
-        return len(self.listdata) 
- 
-    def data(self, index, role): 
-        if index.isValid() and role == QtCore.Qt.DisplayRole:
-            return QtCore.QVariant(self.listdata[index.row()])
-        else: 
-            return QtCore.QVariant()
