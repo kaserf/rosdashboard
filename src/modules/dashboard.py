f02dda51063cc0740ee64c0f3c115bdfd7ee99d1
Felix Kaser <f.kaser@gmx.net>
2012-06-26 13:48:51 +1200

    add functionality to remove widgets from dashboard
    android-home-screen-style remove widget bar.
    
    fixes issue #9

--- src/modules/dashboard.py
+++ src/modules/dashboard.py
@@ -1,24 +1,41 @@
 from PyQt4 import QtGui, QtCore
 from modules.widgets import DashboardWidget
 
-class Dashboard(QtGui.QGroupBox):
+class Dashboard(QtGui.QWidget):
     """ canvas where widgets can be positioned """ 
       
     def __init__(self, parent):
         super(Dashboard, self).__init__(parent)
 
+        #The dashboard keeps track of the widgets that are positioned in the widget container
         self.widgets = list()
         self.initUI()
         
     def initUI(self):
 
-        self.setTitle('Dashboard')
-        self.setAcceptDrops(True)
+        self.title = QtGui.QLabel('Dashboard', self)
+        self.title.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        
+        self.widgetContainer = WidgetContainer(self)
+        self.widgetContainer.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        
+        # set up a bar to drop widgets to remove them
+        self.removeWidgetBar = RemoveWidgetBar(self)
+        self.removeWidgetBar.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        #hide by default
+        self.removeWidgetBar.hide()
+        
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.widgetContainer)
+        self.layout.addWidget(self.removeWidgetBar)
+        self.setLayout(self.layout)
+        
         
     def addWidget(self, widget, position = None):
         """ add widgets to the dashboard """
         if not isinstance(widget, DashboardWidget):
-            raise TypeError("The widget you want to add is not a DashboardWidget")
+            raise TypeError("The widget you want to add is not a DashboardWidget: " + str(widget))
         
         if position == None:
             #TODO: find a free spot and move widget to the free spot
@@ -28,10 +45,74 @@ class Dashboard(QtGui.QGroupBox):
         widget.move(position)
         widget.show()
         
+    def removeWidget(self, widget):
+        """ remove a widget from the dashboard """
+        if not isinstance(widget, DashboardWidget):
+            raise TypeError("The widget you want to remove is not a DashboardWidget: " + str(widget))
+        
+        widget.hide()
+        self.widgets.remove(widget)
+        
+    def showRemoveWidgetBar(self):
+        self.removeWidgetBar.show()
+    
+    def hideRemoveWidgetBar(self):
+        self.removeWidgetBar.hide()
+    
+class RemoveWidgetBar(QtGui.QStatusBar):
+    def __init__(self, parent):
+        super(RemoveWidgetBar, self).__init__(parent)
+        
+        self.parent = parent
+        self.initUI()
+    
+    def initUI(self):
+        self.setSizeGripEnabled(False)
+        self.dropLabel = QtGui.QLabel()
+        self.dropLabel.setText("<font size= '6'>Drag here to delete</font>")
+        self.dropLabel.setStyleSheet("QLabel { background-color: orangered;}")
+        self.dropLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.insertWidget(0, self.dropLabel, 1)
+        self.setAcceptDrops(True)
+        
+    def dragEnterEvent(self, e):
+        
+        #check if the drag comes from a dashboard item
+        if isinstance(e.source(), DashboardWidget):
+            #TODO: show hover effect
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+
+        if isinstance(e.source(), DashboardWidget):
+            self.parent.removeWidget(e.source())
+            e.accept()
+        else:
+            e.ignore()
+            raise TypeError('The source for this kind of drag element is not allowed: ' + str(e.source()))
+        
+        self.parent.hideRemoveWidgetBar()
+
+class WidgetContainer(QtGui.QFrame):
+    def __init__(self, parent):
+        super(WidgetContainer, self).__init__(parent)
+        
+        self.parent = parent
+        self.initUI()
+        
+    def initUI(self):
+        self.setFrameStyle(QtGui.QFrame.Raised | QtGui.QFrame.StyledPanel)
+        self.setAcceptDrops(True)
+        
     def dragEnterEvent(self, e):
         
         #check if the drag comes from a valid item
-        if isinstance(e.source(), DashboardWidget) or isinstance(e.source(), QtGui.QListWidget):
+        if isinstance(e.source(), DashboardWidget):
+            self.parent.showRemoveWidgetBar()
+            e.accept()
+        elif isinstance(e.source(), QtGui.QListWidget):
             e.accept()
         else:
             e.ignore()
@@ -43,11 +124,13 @@ class Dashboard(QtGui.QGroupBox):
             e.source().move(e.pos())
 
             e.setDropAction(QtCore.Qt.MoveAction)
+            
+            self.parent.hideRemoveWidgetBar()
             e.accept()
         elif isinstance(e.source(), QtGui.QListWidget):
             #this happens if a element from the widget list gets dropped
             itemDataInstance = e.source().currentItem().data(QtCore.Qt.UserRole).toPyObject()(self)
-            self.addWidget(itemDataInstance, e.pos())
+            self.parent.addWidget(itemDataInstance, e.pos())
             
             e.accept()
         else:
