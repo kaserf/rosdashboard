c290a2a763ba675955573098fc18deacc169ec2c
Felix Kaser <f.kaser@gmx.net>
2012-07-10 15:59:56 +1200

    finished refactoring
    refactored all the other widgets to follow
    the new generalized approach to do subscriptions.
    
    subscriptions are now handled in the parent class already.
    there is a small interface to the child class (updateValue).

--- src/rosdashboard/widgets/dragCompass.py
+++ src/rosdashboard/widgets/dragCompass.py
@@ -1,23 +1,14 @@
 from PyQt4 import QtGui
-from rosdashboard.modules.props import WidgetProperty
-import rostopic
-import rospy
 from rosdashboard.modules.dashboardWidgets import DashboardWidget
 from PyQt4.Qwt5 import Qwt
 
 class DragCompass(DashboardWidget):
     """ draggable compass"""
-    DATASOURCE = 'datasource'
-    DATAFIELD = 'datafield'
     
     def __init__(self, parent):
         super(DragCompass, self).__init__(parent)
         self.setTitle('DragCompass')
         self.initUI()
-        
-        self.subscriber = None
-        
-        self.initSubscriptions()
         
     def initUI(self):
         self.layout = QtGui.QVBoxLayout()
@@ -32,25 +23,7 @@ class DragCompass(DashboardWidget):
         self.setLayout(self.layout)
         
     def initProps(self):
-        self.props[self.DATASOURCE] = WidgetProperty('text', '/turtle1/pose')
-        self.props[self.DATAFIELD] = WidgetProperty('text', 'linear_velocity')
+        pass
     
-    def propertiesDialogAccepted(self):
-        #re-setup the subscription
-        if (self.subscriber):
-            self.subscriber.unregister()
-        self.initSubscriptions()
-        
-    def initSubscriptions(self):
-        #FIXME: the cast to string is a workaround because subscriber only accepts python strings and not QStrings
-        #introspect the type class of the message
-        data_class = rostopic.get_topic_class(self.props[self.DATASOURCE].value, blocking=False)[0]
-        if data_class:
-            self.subscriber = rospy.Subscriber(str(self.props[self.DATASOURCE].value), data_class, self.subscriptionCallback)
-        else:
-            print "ERROR: could not find data class for this topic: " + self.props[self.DATASOURCE].value
-        
-    def subscriptionCallback(self, data):
-        #FIXME: remove cast to string
-        datafield = getattr(data, str(self.props[self.DATAFIELD].value))
-        self.qwtCompass.setValue(datafield)
+    def updateValue(self, value):
+        self.qwtCompass.setValue(value)
--- src/rosdashboard/widgets/dragString.py
+++ src/rosdashboard/widgets/dragString.py
@@ -1,22 +1,13 @@
 from PyQt4 import QtGui
-from rosdashboard.modules.props import WidgetProperty
-import rostopic
-import rospy
 from rosdashboard.modules.dashboardWidgets import DashboardWidget
 
 class DragString(DashboardWidget):
     """ draggable text field """
-    DATASOURCE = 'datasource'
-    DATAFIELD = 'datafield'
     
     def __init__(self, parent):
         super(DragString, self).__init__(parent)
         self.setTitle('DragString')
         self.initUI()
-        
-        self.subscriber = None
-        
-        self.initSubscriptions()
         
     def initUI(self):
         
@@ -29,27 +20,7 @@ class DragString(DashboardWidget):
         self.setLayout(self.layout)
         
     def initProps(self):
-        self.props[self.DATASOURCE] = WidgetProperty('text', '/turtle1/pose')
-        self.props[self.DATAFIELD] = WidgetProperty('text', 'linear_velocity')
-    
-    def propertiesDialogAccepted(self):
-        #re-setup the subscription
-        if (self.subscriber):
-            self.subscriber.unregister()
-        self.initSubscriptions()
-        
-    def initSubscriptions(self):
-        #FIXME: the cast to string is a workaround because subscriber only accepts python strings and not QStrings
-        #introspect the type class of the message
-        
-        data_class = rostopic.get_topic_class(str(self.props[self.DATASOURCE].value), blocking=False)[0]
-        print str(data_class)
-        if data_class:
-            self.subscriber = rospy.Subscriber(str(self.props[self.DATASOURCE].value), data_class, self.subscriptionCallback)
-        else:
-            print "ERROR: could not find data class for this topic: " + self.props[self.DATASOURCE].value
-        
-    def subscriptionCallback(self, data):
-        #FIXME: remove cast to string
-        datafield = getattr(data, str(self.props[self.DATAFIELD].value))
-        self.textField.setText(str(datafield))
+        pass
+
+    def updateValue(self, value):
+        self.textField.setText(str(value))
