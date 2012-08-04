67c6d1e2b043b7c75f830714b8ba0762e1d9a0cd
Felix Kaser <f.kaser@gmx.net>
2012-08-04 14:30:09 +1200

    added menu for save and load

--- src/rosdashboard/rosdashboard.py
+++ src/rosdashboard/rosdashboard.py
@@ -10,31 +10,66 @@ import QtGui #@UnresolvedImport
 from modules.dashboard import Dashboard
 from modules.toolbox import Toolbox        
         
+class ROSDashboardMain(QtGui.QMainWindow):
+    def __init__(self):
+        super(ROSDashboardMain, self).__init__()
+
+        self.initUi()
+        
+    def initUi(self):
+        saveAction = QtGui.QAction('Save dashboard', self)
+        saveAction.setStatusTip('Save the current dashboard to file')
+        #self.saveAction.triggered.connect(self.showRenameDialog)
+        #self.ctxMenu.addAction(self.renameAction)
+        
+        loadAction = QtGui.QAction('Load dashboard', self)
+        loadAction.setStatusTip('Load a dashboard from file')
+        #self.loadAction.triggered.connect(self.showSubscriptionDialog)
+        #self.ctxMenu.addAction(self.subscriptionAction)
+        
+        exitAction = QtGui.QAction('Exit', self)
+        exitAction.setStatusTip('Exit rosdashboard')
+        exitAction.triggered.connect(QtGui.qApp.quit)
+        
+        fileMenu = self.menuBar().addMenu('File')
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(loadAction)
+        fileMenu.addSeparator()
+        fileMenu.addAction(exitAction)
+        
+        self.resize(800, 600)
+        self.move(100, 100)
+        self.setWindowTitle('Dashboard')
+        
+        centralWidget = QtGui.QWidget()
+        layout = QtGui.QHBoxLayout()
+        
+        toolbox = Toolbox(self)
+        layout.addWidget(toolbox)
+        
+        dashboard = Dashboard(self)
+        layout.addWidget(dashboard)
+        
+        layout.setStretch(0, 20)
+        layout.setStretch(1, 80)
+        
+        centralWidget.setLayout(layout)
+        
+        self.setCentralWidget(centralWidget)
+        self.show()
+        
+    def saveActionTriggered(self):
+        print "save dashboard"
+        
+    def loadActionTriggered(self):
+        print "load dashboard"
+        
 def main():
     
     rospy.init_node('rosdashboard', anonymous=True)
     
     app = QtGui.QApplication(sys.argv)
-    
-    w = QtGui.QWidget()
-    w.resize(800, 600)
-    w.move(100, 100)
-    w.setWindowTitle('Dashboard')
-    
-    layout = QtGui.QHBoxLayout()
-    
-    toolbox = Toolbox(w)
-    layout.addWidget(toolbox)
-    
-    dashboard = Dashboard(w)
-    layout.addWidget(dashboard)
-    
-    layout.setStretch(0, 20)
-    layout.setStretch(1, 80)
-    
-    w.setLayout(layout)
-    w.show()
-    
+    dashboardMain = ROSDashboardMain()
     sys.exit(app.exec_())
     
 
